7758de66f4fb4d717566172a5a77598db0054379
kwoolytech <kwooly.tech@gmail.com>
2016-06-24 07:34:04 +0900

    SimpleAndroidApp

---
+++ app/src/main/java/com/kwoolytech/travis_ci_android_example/MainActivity.java
@@ -0,0 +1,13 @@
+package com.kwoolytech.travis_ci_android_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
